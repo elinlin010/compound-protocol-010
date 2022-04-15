2752c7b164b5b48ae20c0c9f7bee01f7ae2a97bb
Yi-Ling Lin <elinlin010@gmail.com>
2022-04-15 22:20:27 +0800

    Use environment variable in hardhat config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,17 +1,22 @@
 require("@nomiclabs/hardhat-waffle");
+require('dotenv').config();
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
-
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
+//task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+//  const accounts = await hre.ethers.getSigners();
+//
+//  for (const account of accounts) {
+//    console.log(account.address);
+//  }
+//});
 
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
+
+console.log(process.env.INFURA_API_KEY);
+console.log(process.env.PRIVATE_KEY);
+console.log(process.env.ETHERSCAN_API_KEY);
 
 /**
  * @type import('hardhat/config').HardhatUserConfig
@@ -26,4 +31,19 @@ module.exports = {
       }
     }
   },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY
+  },
+  networks: {
+    rinkeby: {
+      //url: `https://eth-rinkeby.alchemyapi.io/v2/${process.env.ALCHEMY_API_KEY}`,
+      url: `https://rinkeby.infura.io/v3/${process.env.INFURA_API_KEY}`,
+      accounts: [`${process.env.PRIVATE_KEY}`]
+    },
+    goerli: {
+      //url: `https://eth-goerli.alchemyapi.io/v2/${process.env.ALCHEMY_API_KEY}`,
+      url: `https://goerli.infura.io/v3/${process.env.INFURA_API_KEY}`,
+      accounts: [`${process.env.PRIVATE_KEY}`]
+    }
+  }
 };
